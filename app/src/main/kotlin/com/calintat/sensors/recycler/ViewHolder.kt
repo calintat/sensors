8d87cd2efc15130bf6c2028ada6d42fd7c61af2b
calintat <calintat@gmail.com>
2017-04-15 18:26:59 +0100

    Added bind method

--- app/src/main/kotlin/com/calintat/sensors/recycler/ViewHolder.kt
+++ app/src/main/kotlin/com/calintat/sensors/recycler/ViewHolder.kt
@@ -4,11 +4,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import com.calintat.sensors.R
+import com.calintat.sensors.api.Snapshot
 import org.jetbrains.anko.find
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    val time = itemView.find<TextView>(R.id.logger_list_item_time)
+    private val time = itemView.find<TextView>(R.id.logger_list_item_time)
 
-    val data = itemView.find<TextView>(R.id.logger_list_item_data)
+    private val data = itemView.find<TextView>(R.id.logger_list_item_data)
+
+    fun bindItem(item: Snapshot) {
+
+        time.text = item.time.toString()
+
+        data.text = item.data.joinToString(separator = "\n")
+    }
 }
