eac996806fa1f302c54417f539fc2d7a310c66ab
calintat <calintat@gmail.com>
2017-04-15 18:27:48 +0100

    Updated recycler view adapter

--- app/src/main/kotlin/com/calintat/sensors/recycler/Adapter.kt
+++ app/src/main/kotlin/com/calintat/sensors/recycler/Adapter.kt
@@ -2,41 +2,31 @@ package com.calintat.sensors.recycler
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.calintat.sensors.R
+import com.calintat.sensors.api.Snapshot
+import com.calintat.sensors.ui.ListItem
+import org.jetbrains.anko.AnkoContext
 
 class Adapter(private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
-    val items = mutableListOf<Pair<Float, FloatArray>>()
+    val items = mutableListOf<Snapshot>()
 
-    fun add(time: Float, measurement: FloatArray) {
+    fun add(item: Snapshot) {
 
-        items.add(0, Pair(time, measurement))
+        items.add(0, item)
 
         notifyItemInserted(0)
     }
 
-    override fun getItemCount(): Int {
-
-        return items.size
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val (time, measurement) = items[position]
-
-        holder.time.text = time.toString()
-
-        holder.measurement.text = measurement.joinToString(separator = "\n")
+        holder.bindItem(items[position])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val layoutInflater = LayoutInflater.from(context)
-
-        val layoutResource = R.layout.fragment_logger_list_item
-
-        return ViewHolder(layoutInflater.inflate(layoutResource, parent, false))
+        return ViewHolder(ListItem.createView(AnkoContext.create(context, parent)))
     }
 }
