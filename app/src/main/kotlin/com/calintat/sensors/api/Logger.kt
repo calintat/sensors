65560781837a3f1dea1d5079ceeb13a346d19086
calintat <calintat@gmail.com>
2017-04-29 23:09:59 +0100

    Reformatted the code logger fragment

--- app/src/main/kotlin/com/calintat/sensors/api/Logger.kt
+++ app/src/main/kotlin/com/calintat/sensors/api/Logger.kt
@@ -1,85 +1,70 @@
 package com.calintat.sensors.api
 
-import android.app.Fragment
-import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.calintat.sensors.R
 import com.calintat.sensors.api.Snapshot.Companion.snap
 import com.calintat.sensors.recycler.Adapter
-import com.calintat.sensors.ui.MainUI.textAppearance
+import com.calintat.sensors.utils.AnkoUtils
+import com.calintat.sensors.utils.AnkoUtils.textAppearance
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 /**
  * Fragment which acts as a data logger for sensor snapshots.
  */
-class Logger : Fragment() {
+class Logger : AnkoUtils.AnkoFragment() {
 
-    private object LoggerComponent : AnkoComponent<Logger> {
+    var adapter: Adapter? = null
 
-        override fun createView(ui: AnkoContext<Logger>) = with(ui) {
+    val isEmpty get() = adapter?.items?.isEmpty()
 
-            verticalLayout {
+    val timestamp by lazy { System.currentTimeMillis() }
 
-                linearLayout {
+    fun log(xs: List<Float>) = adapter?.add(timestamp snap xs)
 
-                    orientation = LinearLayout.HORIZONTAL
+    override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
 
-                    textView {
+        adapter = Adapter(activity)
 
-                        textResource = R.string.time
+        verticalLayout {
 
-                        gravity = Gravity.CENTER
+            linearLayout {
 
-                        textAppearance = R.style.TextAppearance_AppCompat_Subhead
+                lparams(width = matchParent, height = dip(56))
 
-                    }.lparams(width = 0, height = matchParent, weight = 1f)
+                orientation = LinearLayout.HORIZONTAL
 
-                    textView {
+                textView(text = R.string.time) {
 
-                        textResource = R.string.data
+                    lparams(width = 0, height = matchParent, weight = 1f)
 
-                        gravity = Gravity.CENTER
+                    gravity = Gravity.CENTER
 
-                        textAppearance = R.style.TextAppearance_AppCompat_Subhead
+                    textAppearance = R.style.TextAppearance_AppCompat_Subhead
 
-                    }.lparams(width = 0, height = matchParent, weight = 1f)
+                }
 
-                }.lparams(width = matchParent, height = dip(56))
+                textView(text = R.string.data) {
 
-                recyclerView {
+                    lparams(width = 0, height = matchParent, weight = 1f)
 
-                    adapter = owner.adapter
+                    gravity = Gravity.CENTER
 
-                    layoutManager = LinearLayoutManager(ctx)
+                    textAppearance = R.style.TextAppearance_AppCompat_Subhead
+                }
+            }
 
-                }.lparams(width = matchParent, height = 0, weight = 1f)
+            recyclerView {
+
+                lparams(width = matchParent, height = 0, weight = 1f)
+
+                adapter = this@Logger.adapter
+
+                layoutManager = LinearLayoutManager(ctx)
             }
         }
-    }
-
-    private val adapter by lazy { Adapter(activity) }
-
-    private val currentTime get() = (System.currentTimeMillis() - initialTime) / 1000f
-
-    var isEmpty = true
-
-    val entries get() = adapter.items
-
-    val initialTime by lazy { System.currentTimeMillis() }
-
-    operator fun plusAssign(data: List<Float>?) {
-
-        data?.let { isEmpty = false; adapter.add(currentTime snap it) }
-    }
-
-    override fun onCreateView(i: LayoutInflater?, c: ViewGroup?, s: Bundle?): View {
-
-        return LoggerComponent.createView(AnkoContext.create(activity, this))
     }
 }
