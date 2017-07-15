3c03065859a6c69b11c98eaada01f71242fadc9f
calintat <calintat@gmail.com>
2017-07-15 17:44:41 +0100

    Preferences are now declared with the Alps DSL

--- app/src/main/kotlin/com/calintat/sensors/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/calintat/sensors/activities/SettingsActivity.kt
@@ -3,10 +3,12 @@ package com.calintat.sensors.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.calintat.sensors.R
+import com.calintat.sensors.api.Item
 import com.calintat.sensors.ui.SettingsUI
-import com.calintat.sensors.ui.SettingsUI.toolbar
-import com.github.calintat.populateWithPreferences
+import com.calintat.sensors.utils.ShortcutsUtils.shortcuts
+import com.github.calintat.*
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.toast
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -14,14 +16,74 @@ class SettingsActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
 
-        SettingsUI.setContentView(this)
+        SettingsUI().setContentView(this)
 
-        toolbar.setTitle(R.string.navigation_settings)
+        populateWithPreferences(R.id.container) {
 
-        toolbar.setNavigationIcon(R.drawable.ic_action_back)
+            listPreference("pref_theme") {
 
-        toolbar.setNavigationOnClickListener { finish() }
+                setTitle(R.string.pref_theme)
 
-        populateWithPreferences(R.id.fragment, R.xml.preferences)
+                summary = "%s"
+
+                entries = arrayOf("Auto", "Light", "Dark")
+
+                entryValues = arrayOf("0", "1", "2")
+
+                setOnPreferenceChangeListener { _, _ -> toast(R.string.msg_app_restart_required); true }
+            }
+
+            multiSelectListPreference {
+
+                setTitle(R.string.pref_shortcuts)
+
+                values = shortcuts.toSet()
+
+                entries = Item.values().map { resources.getString(it.label) }.toTypedArray()
+
+                entryValues = Item.values().map { it.shortcutId }.toTypedArray()
+
+                setOnPreferenceChangeListener { _, v -> onShortcutsChanged(v as? Set<String>) }
+            }
+
+            preferenceCategory {
+
+                setTitle(R.string.pref_about)
+
+                preference {
+
+                    setTitle(R.string.pref_version)
+
+                    setSummary(R.string.app_version)
+
+                    setUrl("market://details?id=com.calintat.sensors")
+                }
+
+                preference {
+
+                    setTitle(R.string.pref_developer)
+
+                    setSummary(R.string.app_developer)
+
+                    setUrl("https://play.google.com/store/apps/dev?id=5526451977947367946")
+                }
+            }
+        }
+    }
+
+    internal fun onShortcutsChanged(new: Set<String>?): Boolean {
+
+        if (new != null && new.size <= 4) {
+
+            shortcuts = new.toList()
+
+            return true
+        }
+        else {
+
+            toast(R.string.err_shortcuts)
+
+            return false
+        }
     }
 }
