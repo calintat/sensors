ed9fd3f7c1db68c0d71797b41d76c48a8e92a61d
calintat <calintat@gmail.com>
2017-07-15 17:51:44 +0100

    Added useful Anko fields

--- app/src/main/kotlin/com/calintat/sensors/utils/AnkoUtils.kt
+++ app/src/main/kotlin/com/calintat/sensors/utils/AnkoUtils.kt
@@ -1,39 +1,31 @@
 package com.calintat.sensors.utils
 
-import android.app.Fragment
 import android.os.Build
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.StyleRes
+import android.support.v4.content.ContextCompat
+import android.support.v7.content.res.AppCompatResources
+import android.support.v7.widget.Toolbar
 import android.widget.TextView
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.PropertyWithoutGetterException
 
 object AnkoUtils {
 
+    var Toolbar.titleTextColorResource: Int
+
+        get() = throw Exception("No getter")
+
+        set(@ColorRes value) { setTitleTextColor(ContextCompat.getColor(context, value)) }
+
+    var Toolbar.overflowIconResource: Int
+
+        get() = throw Exception("No getter")
+
+        set(@DrawableRes value) { overflowIcon = AppCompatResources.getDrawable(context, value) }
+
     @Suppress("DEPRECATION") var TextView.textAppearance: Int
 
-        get() = throw PropertyWithoutGetterException("textAppearance")
+        get() = throw Exception("No getter")
 
-        set(value) {
-
-            if (Build.VERSION.SDK_INT >= 23) {
-
-                setTextAppearance(value)
-            }
-            else {
-
-                setTextAppearance(context, value)
-            }
-        }
-
-    abstract class AnkoFragment : Fragment(), AnkoComponent<ViewGroup> {
-
-        final override fun onCreateView(i: LayoutInflater?, c: ViewGroup, s: Bundle?): View {
-
-            return createView(AnkoContext.create(activity, c))
-        }
-    }
+        set(@StyleRes value) { if (Build.VERSION.SDK_INT >= 23) setTextAppearance(value) else setTextAppearance(context, value) }
 }
