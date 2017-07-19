6e807763f5f0190556f6d17af41565b4afccbbab
calintat <calintat@gmail.com>
2017-07-19 17:11:50 +0100

    Only setter suppresses deprecation warning

--- app/src/main/java/com/calintat/sensors/utils/AnkoProperties.kt
+++ app/src/main/java/com/calintat/sensors/utils/AnkoProperties.kt
@@ -1,9 +1,6 @@
 package com.calintat.sensors.utils
 
 import android.os.Build
-import android.support.annotation.ColorRes
-import android.support.annotation.DrawableRes
-import android.support.annotation.StyleRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.content.res.AppCompatResources
 import android.support.v7.widget.Toolbar
@@ -16,17 +13,17 @@ object AnkoProperties {
 
         @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
-        set(@ColorRes value) { setTitleTextColor(ContextCompat.getColor(context, value)) }
+        set(value) { setTitleTextColor(ContextCompat.getColor(context, value)) }
 
     var Toolbar.overflowIconResource: Int
 
         @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
-        set(@DrawableRes value) { overflowIcon = AppCompatResources.getDrawable(context, value) }
+        set(value) { overflowIcon = AppCompatResources.getDrawable(context, value) }
 
-    @Suppress("DEPRECATION") var TextView.textAppearance: Int
+    var TextView.textAppearance: Int
 
         @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
-        set(@StyleRes value) { if (Build.VERSION.SDK_INT >= 23) setTextAppearance(value) else setTextAppearance(context, value) }
+        @Suppress("DEPRECATION") set(value) { if (Build.VERSION.SDK_INT >= 23) setTextAppearance(value) else setTextAppearance(context, value) }
 }
