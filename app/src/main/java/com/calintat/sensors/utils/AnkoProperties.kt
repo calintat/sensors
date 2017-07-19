a67bbaeb4700bcddc903f1aa2d59c0d482c2f2bf
calintat <calintat@gmail.com>
2017-07-19 17:08:39 +0100

    Getters are now deprecated at error level

--- app/src/main/java/com/calintat/sensors/utils/AnkoProperties.kt
+++ app/src/main/java/com/calintat/sensors/utils/AnkoProperties.kt
@@ -8,24 +8,25 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.content.res.AppCompatResources
 import android.support.v7.widget.Toolbar
 import android.widget.TextView
+import org.jetbrains.anko.internals.AnkoInternals
 
 object AnkoProperties {
 
     var Toolbar.titleTextColorResource: Int
 
-        get() = throw Exception("No getter")
+        @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
         set(@ColorRes value) { setTitleTextColor(ContextCompat.getColor(context, value)) }
 
     var Toolbar.overflowIconResource: Int
 
-        get() = throw Exception("No getter")
+        @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
         set(@DrawableRes value) { overflowIcon = AppCompatResources.getDrawable(context, value) }
 
     @Suppress("DEPRECATION") var TextView.textAppearance: Int
 
-        get() = throw Exception("No getter")
+        @Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
 
         set(@StyleRes value) { if (Build.VERSION.SDK_INT >= 23) setTextAppearance(value) else setTextAppearance(context, value) }
 }
