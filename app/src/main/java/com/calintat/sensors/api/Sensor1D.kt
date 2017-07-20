1365f8d9baf891d099a38fac20dc36ae7e0e56db
calintat <calintat@gmail.com>
2017-07-20 22:58:36 +0100

    Sensor1D and Sensor3D are now AnkoComponents

--- app/src/main/java/com/calintat/sensors/api/Sensor1D.kt
+++ app/src/main/java/com/calintat/sensors/api/Sensor1D.kt
@@ -1,25 +1,16 @@
 package com.calintat.sensors.api
 
 import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.TextView
 import com.calintat.sensors.R
 import com.calintat.sensors.utils.AnkoProperties.textAppearance
 import org.jetbrains.anko.*
 
 /**
- * Implementation of the [Sensor] class for 1-dimensional sensors.
+ * Anko UI for the [Sensor] fragment for 1-dimensional sensors.
  */
-class Sensor1d : Sensor() {
+object Sensor1D : AnkoComponent<Sensor> {
 
-    internal var field: TextView? = null
-
-    override fun onValuesChanged(newValues: FloatArray) {
-
-        field?.text = newValues[0].toString()
-    }
-
-    override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+    override fun createView(ui: AnkoContext<Sensor>) = with(ui) {
 
         verticalLayout {
 
@@ -27,7 +18,7 @@ class Sensor1d : Sensor() {
 
             gravity = Gravity.CENTER
 
-            field = textView {
+            owner.textViews += textView {
 
                 maxLines = 1
 
@@ -35,7 +26,7 @@ class Sensor1d : Sensor() {
 
             }.lparams(width = wrapContent, height = wrapContent)
 
-            textView(text = item.unit) {
+            textView(text = owner.item.unit) {
 
                 textAppearance = R.style.TextAppearance_AppCompat_Body2
 
--- app/src/main/java/com/calintat/sensors/api/Sensor3D.kt
+++ app/src/main/java/com/calintat/sensors/api/Sensor3D.kt
@@ -2,7 +2,6 @@ package com.calintat.sensors.api
 
 import android.support.annotation.StringRes
 import android.view.Gravity
-import android.view.ViewGroup
 import android.view.ViewManager
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -11,26 +10,11 @@ import com.calintat.sensors.utils.AnkoProperties.textAppearance
 import org.jetbrains.anko.*
 
 /**
- * Implementation of the [Sensor] class for 3-dimensional sensors.
+ * Anko UI for the [Sensor] fragment for 3-dimensional sensors.
  */
-class Sensor3d : Sensor() {
+object Sensor3D : AnkoComponent<Sensor> {
 
-    internal var fieldX: TextView? = null
-
-    internal var fieldY: TextView? = null
-
-    internal var fieldZ: TextView? = null
-
-    override fun onValuesChanged(newValues: FloatArray) {
-
-        fieldX?.text = newValues[0].toString()
-
-        fieldY?.text = newValues[1].toString()
-
-        fieldZ?.text = newValues[2].toString()
-    }
-
-    override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+    override fun createView(ui: AnkoContext<Sensor>) = with(ui) {
 
         linearLayout {
 
@@ -44,15 +28,15 @@ class Sensor3d : Sensor() {
 
             padding = dip(16)
 
-            createField(R.string.x_axis) { fieldX = this }
+            createField(R.string.x_axis, owner.item.unit) { owner.textViews += this }
 
-            createField(R.string.y_axis) { fieldY = this }
+            createField(R.string.y_axis, owner.item.unit) { owner.textViews += this }
 
-            createField(R.string.z_axis) { fieldZ = this }
+            createField(R.string.z_axis, owner.item.unit) { owner.textViews += this }
         }
     }
 
-    internal fun ViewManager.createField(@StringRes textResId: Int, init: TextView.() -> Unit) {
+    internal fun ViewManager.createField(@StringRes top: Int, @StringRes bottom: Int, init: TextView.() -> Unit) {
 
         verticalLayout {
 
@@ -60,7 +44,7 @@ class Sensor3d : Sensor() {
 
             gravity = Gravity.CENTER
 
-            textView(text = textResId) {
+            textView(text = top) {
 
                 textAppearance = R.style.TextAppearance_AppCompat_Caption
 
@@ -72,11 +56,13 @@ class Sensor3d : Sensor() {
 
                 maxLines = 1
 
+                horizontalPadding = dip(10)
+
                 textAppearance = R.style.TextAppearance_AppCompat_Title
 
             }.lparams(width = wrapContent, height = wrapContent)
 
-            textView(text = item.unit) {
+            textView(text = bottom) {
 
                 textAppearance = R.style.TextAppearance_AppCompat_Body2
 
